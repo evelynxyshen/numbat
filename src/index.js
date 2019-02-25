d972716037965d9cff1f87fa335bd5e3359b6a49
Evelyn Shen <evelyn.shen@airbnb.com>
2019-02-25 00:02:00 -0800

    add layout

--- src/index.js
+++ src/index.js
@@ -1,47 +1,37 @@
-import express from 'express';
 import path from "path";
 
+import React from "react";
+import { renderToString } from "react-dom/server";
+import Layout from "./components/Layout";
+
 const app = express();
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to Node.js & Express' });
-});
-app.listen(process.env.PORT || 5000, () => console.log("Listening to port 5000"));
+app.use( express.static( path.resolve( __dirname, "public" ) ) );
 
-// import path from "path";
-  // 
-  // import React from "react";
-  // import { renderToString } from "react-dom/server";
-  // import Layout from "./components/Layout";
-  // 
-  // const app = express();
-  // const PORT = process.env.PORT || 5000
-  // 
-  // app.use( express.static( path.resolve( __dirname, "public" ) ) );
-  // 
-  // app.get( "/*", ( req, res ) => {
-  //   const jsx = ( <Layout /> );
-  //   const reactDom = renderToString( jsx );
-  // 
-  //   res.writeHead( 200, { "Content-Type": "text/html" } );
-  //   res.end( htmlTemplate( reactDom ) );
-  // } );
-  // 
-  // app.listen( PORT, () => console.log(`Listening on ${ PORT }`) );
-  // 
-  // function htmlTemplate( reactDom ) {
-  //   return `
-  //         <!DOCTYPE html>
-  //         <html>
-  //         <head>
-  //             <meta charset="utf-8">
-  //             <title>React SSR</title>
-  //         </head>
-  // 
-  //         <body>
-  //             <div id="app">${ reactDom }</div>
-  //             <script src="./app.bundle.js"></script>
-  //         </body>
-  //         </html>
-  //   `;
-  // }
+app.get( "/*", ( req, res ) => {
+  const jsx = ( <Layout /> );
+  const reactDom = renderToString( jsx );
+
+  res.writeHead( 200, { "Content-Type": "text/html" } );
+  res.end( htmlTemplate( reactDom ) );
+} );
+
+app.listen( PORT, () => console.log(`Listening on ${ PORT }`) );
+
+function htmlTemplate( reactDom ) {
+  return `
+         <!DOCTYPE html>
+         <html>
+         <head>
+             <meta charset="utf-8">
+             <title>React SSR</title>
+         </head>
+
+         <body>
+             <div id="app">${ reactDom }</div>
+             <script src="./app.bundle.js"></script>
+         </body>
+         </html>
+  `;
+}
